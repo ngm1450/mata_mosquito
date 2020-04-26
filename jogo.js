b8780891c5f81108a93061d8b99682a19b60b214
Nicolas Georgeos Mantzos <georgeos.nicolas83@gmail.com>
2020-04-26 13:43:49 -0300

    Adição do arquivo de estilo e configuração de posições randômicas.

--- jogo.js
+++ jogo.js
@@ -5,8 +5,30 @@ var largura = 0
 function ajustarTamanhoPalcoJogo() {
 	altura = window.innerHeight
 	largura = window.innerWidth
-
 	console.log(altura, largura)
 }
 
 ajustarTamanhoPalcoJogo()
+
+function posicionarRandomicamente(){
+    
+    var posicaoX = Math.floor(Math.random()*largura) - 90
+    var posicaoY = Math.floor(Math.random()*altura) - 90
+
+    popsiçãoX = posicaoX < 0 ? 0 : posicaoX
+    popsiçãoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //criação do elemento html dinamicamente
+    var mosquito = document.createElement('img')
+    mosquito.src = 'img/mosquito.png'
+    mosquito.className = 'mosquito1'
+
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    
+    document.body.appendChild(mosquito)
+}
